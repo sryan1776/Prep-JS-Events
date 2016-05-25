24346660ebf623553265a5efb2690a247bd74a6b
Shawna Ryan <Shawna@silverfox.local>
2016-05-24 19:23:30 -1000

    exercise 5

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
 
   /*1.  Fill in the correct id to target the paragraph tag*/
   function fontSize(){
-    document.getElementById().style.fontSize = "200%";
+    document.getElementById("biggie").style.fontSize = "200%";
   }
 
  /* 2. change the font color to red*/
   function colorMeBadd(){
-    document.getElementById().style.color = something; 
+    document.getElementById("colors").style.color = "red"; 
   }
 
 /*3. fill in the spaces to make this function operate:*/
   function showAnswer(){
-    document.getElementById().something = " C (da sea get it?) arrrr!";
+    document.getElementById("answer").innerHTML = " C (da sea get it?) arrrr!";
   }
 
   /*4.  Create a button that will display the message "My name is Lee, Brrruuuce Lee!" in a new paragraph element after the button is clicked.*/
-function something(){
-  document.getElementById(something).something = "My name is Lee, B<span>rrr</span>uuuce Lee!";
+function kungfu(){
+  document.getElementById("beWater").innerHTML = "My name is Lee, B<span>rrr</span>uuuce Lee!";
 }
  
 //Event Listeners
 //Please write your code inside the window.onload function for exerices 5 onward...
 
-window.onload = function (){
+window.onload = function button (){
 
-document.getElementById().addEventListener('click', something);
+document.getElementById("Benjamin").addEventListener('click', sayQuote);
 
 function sayQuote(){
-  something.getElementById().something = "Our lives are defined by opportunities; even the ones we miss.";
+  document.getElementById("lines").innerHTML = "Our lives are defined by opportunities; even the ones we miss.";
 }
 
 
 //6.  Generate a random quote from clicking on a button
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance."];
 
-document.getElementById().something();
+document.getElementById("change").addEventListener("displayQuotes", sayQuotes);
                                                    
 function sayQuotes(){
   var getQuotes = quotes[Math.floor(Math.random()*quotes.length)];
